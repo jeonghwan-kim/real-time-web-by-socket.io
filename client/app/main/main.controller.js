4a5070f2f7af98fd781aed1edcdacf957cad259b
jeonghwan <ej88ej@gmail.com>
2015-07-06 22:42:36 +0900

    Install angular-fullstack

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('realTimeWebBySocketioApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+  });
---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('realTimeWebBySocketioApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
